14079eebfe1caccc2973d276b49349b42584c9eb
sspano01 <51494130+sspano01@users.noreply.github.com>
2020-05-05 11:52:10 -0400

    fixup signtool for new cert

---
+++ Installer/sign.bat
@@ -0,0 +1,1 @@
+"c:\Program Files\Microsoft SDKs\Windows\V7.1\bin\SignTool.exe" sign /s my /sha1 8C79A8322A8B6138BC3F96CF1EB426A774921183 /fd sha256 /tr http://sha256timestamp.ws.symantec.com/sha256/timestamp /v  %2
