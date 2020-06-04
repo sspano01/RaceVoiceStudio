5784db659c149c0e86ddbc92213666ee744b7657
sspano01 <51494130+sspano01@users.noreply.github.com>
2020-06-04 12:57:43 -0400

    rebuild on apple

--- RaceVoice/TrackRenderer.cs
+++ RaceVoice/TrackRenderer.cs
@@ -455,8 +455,10 @@ namespace RaceVoice
        public void Render(Bitmap renderTarget)
 #endif
         {
-            if (renderTarget == null) return;
 
+#if !APP
+            if (renderTarget == null) return;
+#endif
             if (_transformedSegments == null)
             {
                 _transformedSegments = new List<PointF[]>(_model.Segments.Count);
